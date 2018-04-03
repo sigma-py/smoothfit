a30e7028cff58c34652501f71ffbe597da24cb5e
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-03 16:13:55 +0200

    more scaffolding

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, 'smoothfit', '__about__.py'), 'rb') as f:
+    # pylint: disable=exec-used
+    exec(f.read(), about)
+
+
+def read(fname):
+    return codecs.open(os.path.join(base_dir, fname), encoding='utf-8').read()
+
+
+setup(
+    name='smoothfit',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    packages=find_packages(),
+    description='Smooth data fitting',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nschloe/smoothfit',
+    project_urls={
+        'Issues': 'https://github.com/nschloe/smoothfit/issues',
+        },
+    license=about['__license__'],
+    platforms='any',
+    install_requires=[
+        'numpy',
+        'pipdate',
+        ],
+    classifiers=[
+        about['__status__'],
+        about['__license__'],
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering'
+        ]
+    )
