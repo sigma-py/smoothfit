57c7174e9ea041236183b5bac8ae1919f95ec669
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-24 22:20:08 +0200

    more experiments with preconditioner

--- experimental/prec_solver.py
+++ experimental/prec_solver.py
@@ -27,8 +27,8 @@ def _assemble_eigen(form, bcs=None):
     return L
 
 
-def solve(M, b, mesh, Eps):
-    V = FunctionSpace(mesh, 'CG', 1)
+def solve(mesh, Eps, degree):
+    V = FunctionSpace(mesh, 'CG', degree)
     u = TrialFunction(V)
     v = TestFunction(V)
 
@@ -36,19 +36,16 @@ def solve(M, b, mesh, Eps):
 
     dim = mesh.geometry().dim()
 
-    A = [[
+    A = [
         _assemble_eigen(
             + Constant(Eps[i, j]) * u.dx(i) * v.dx(j) * dx
-            # pylint: disable=unsubscriptable-object
             - Constant(Eps[i, j]) * u.dx(i) * n[j] * v * ds
             ).sparray()
         for j in range(dim)
-        ]
         for i in range(dim)
         ]
-    Aflat = [item for sublist in A for item in sublist]
 
-    assert_equality = False
+    assert_equality = True
     if assert_equality:
         # The sum of the `A`s is exactly that:
         n = FacetNormal(V.mesh())
@@ -56,10 +53,10 @@ def solve(M, b, mesh, Eps):
             + dot(dot(as_tensor(Eps), grad(u)), grad(v)) * dx
             - dot(dot(as_tensor(Eps), grad(u)), n) * v * ds
             ).sparray()
-        diff = AA - sum(Aflat)
+        diff = AA - sum(A)
         assert numpy.all(abs(diff.data) < 1.0e-14)
         #
-        # ATAsum = sum(a.T.dot(a) for a in Aflat)
+        # ATAsum = sum(a.T.dot(a) for a in A)
         # diff = AA.T.dot(AA) - ATAsum
         # # import betterspy
         # # betterspy.show(ATAsum)
@@ -68,68 +65,78 @@ def solve(M, b, mesh, Eps):
         # print(diff.data)
         # assert numpy.all(abs(diff.data) < 1.0e-14)
 
-    tol=1.0e-13
+    tol=1.0e-10
 
     def lower(x, on_boundary):
         return on_boundary and abs(x[1] + 1.0) < tol
+
+    def upper(x, on_boundary):
+        return on_boundary and abs(x[1] - 1.0) < tol
+
+    def left(x, on_boundary):
+        return on_boundary and abs(x[0] + 1.0) < tol
 
     def right(x, on_boundary):
         return on_boundary and abs(x[0] - 1.0) < tol
 
     def upper_left(x, on_boundary):
-        return on_boundary and (abs(x[0] + 1.0) < tol) and (abs(x[1] - 1.0)<tol)
-
-    def lower_left(x, on_boundary):
-        return on_boundary and (abs(x[0] + 1.0) < tol) and (abs(x[1] + 1.0)<tol)
+        return on_boundary and x[1] > +1.0 - tol and x[0] < -0.8
 
     def lower_right(x, on_boundary):
-        return on_boundary and (abs(x[0] - 1.0) < tol) and (abs(x[1] + 1.0)<tol)
+        return on_boundary and x[1] < -1.0 + tol and x[0] > 0.8
 
     bcs = [
-        DirichletBC(V, Constant(0.0), upper_left, method='pointwise'),
-        DirichletBC(V, Constant(0.0), lower_left, method='pointwise'),
-        DirichletBC(V, Constant(0.0), lower_right, method='pointwise'),
+        DirichletBC(V, Constant(0.0), lower_right),
+        DirichletBC(V, Constant(0.0), upper_left),
+        # DirichletBC(V, Constant(0.0), left),
+        # DirichletBC(V, Constant(0.0), right),
+        # DirichletBC(V, Constant(0.0), upper_left, method='pointwise'),
+        # DirichletBC(V, Constant(0.0), lower_left, method='pointwise'),
+        # DirichletBC(V, Constant(0.0), lower_right, method='pointwise'),
         ]
+
+    M = _assemble_eigen(u*v*dx).sparray()
+
+    ATMinvAsum = sum(
+        numpy.dot(a.toarray().T, numpy.linalg.solve(M.toarray(), a.toarray()))
+        for a in A
+        )
 
     AA2 = _assemble_eigen(
         + dot(dot(as_tensor(Eps), grad(u)), grad(v)) * dx
         - dot(dot(as_tensor(Eps), grad(u)), n) * v * ds,
         # bcs=[DirichletBC(V, Constant(0.0), 'on_boundary')]
-        # bcs=bcs
-        bcs=[
-            DirichletBC(V, Constant(0.0), lower),
-            DirichletBC(V, Constant(0.0), right),
-            ]
+        bcs=bcs
+        # bcs=[
+        #     DirichletBC(V, Constant(0.0), lower),
+        #     DirichletBC(V, Constant(0.0), right),
+        #     ]
         ).sparray()
 
-    ATA2 = AA2.dot(AA2)
-    # ATAsum = sum(a.T.dot(a) for a in Aflat)
+    ATA2 = AA2.T.dot(numpy.linalg.solve(M.toarray(), AA2.toarray()))
 
-    # ATAsum_eigs = numpy.sort(numpy.linalg.eigvalsh(ATAsum.todense()))
-    # print(ATAsum_eigs)
-    # print()
-    ATA2_eigs = numpy.sort(numpy.linalg.eigvalsh(ATA2.todense()))
-    print(ATA2_eigs)
-    exit(1)
-    # plt.semilogy(range(len(ATAsum_eigs)), ATAsum_eigs, '.', label='ATAsum')
-    # plt.semilogy(range(len(ATA2_eigs)), ATA2_eigs, '.', label='ATA2')
-    # plt.legend()
-    # plt.show()
+    # Eigenvalues of the operators
+    if True:
+        ATMinvAsum_eigs = numpy.sort(numpy.linalg.eigvalsh(ATMinvAsum))
+        ATA2_eigs = numpy.sort(numpy.linalg.eigvalsh(ATA2))
+        # print(ATA2_eigs[:20])
+        # exit(1)
+        plt.semilogy(ATMinvAsum_eigs, '.', label='ATMinvAsum')
+        plt.semilogy(ATA2_eigs, '.', label='ATA2')
+        plt.legend()
+        plt.show()
 
-    # # invsqrtATA2 = numpy.linalg.inv(scipy.linalg.sqrtm(ATA2.todense()))
-    # # IATA = numpy.dot(numpy.dot(invsqrtATA2, ATAsum), invsqrtATA2)
-    # # IATA_eigs = numpy.sort(numpy.linalg.eigvals(IATA))
-    # IATA_eigs = numpy.sort(scipy.linalg.eigvals(ATAsum.todense(), ATA2.todense()))
-    # plt.semilogy(range(len(IATA_eigs)), IATA_eigs, '.', label='IATA')
-    # # plt.plot(IATA_eigs, numpy.zeros(len(IATA_eigs)), 'x', label='IATA')
-    # plt.legend()
-    # plt.show()
-    # # exit(1)
+    # Preconditioned eigenvalues
+    if True:
+        IATA_eigs = numpy.sort(scipy.linalg.eigvalsh(ATMinvAsum, ATA2))
+        plt.semilogy(IATA_eigs, '.', label='precond eigenvalues')
+        plt.legend()
+        plt.show()
 
     # # Test with A only
-    # M = sparse.vstack(Aflat)
+    # M = sparse.vstack(A)
     # numpy.random.seed(123)
-    # b = numpy.random.rand(sum(a.shape[0] for a in Aflat))
+    # b = numpy.random.rand(sum(a.shape[0] for a in A))
     # MTM = M.T.dot(M)
     # MTb = M.T.dot(b)
     # sol = _gmres(
@@ -180,11 +187,31 @@ def solve(M, b, mesh, Eps):
     linear_system = krypy.linsys.LinearSystem(MTM, M.T.dot(b), M=prec)
     out = krypy.linsys.Gmres(linear_system, tol=1.0e-12)
 
-    # plt.semilogy(out.resnorms)
-    # plt.show()
+    plt.semilogy(out.resnorms)
+    plt.show()
 
     return out.xk
 
 
 if __name__ == '__main__':
-    dla
+    # # 1d mesh
+    # mesh = IntervalMesh(20, -1.0, +1.0)
+    # Eps = numpy.array([[1.0]])
+
+    # 2d mesh
+    import meshzoo
+    points, cells = meshzoo.rectangle(-1.0, 1.0, -1.0, 1.0, 20, 20)
+    editor = MeshEditor()
+    mesh = Mesh()
+    # topological and geometrical dimension 2
+    editor.open(mesh, 'triangle', 2, 2, 1)
+    editor.init_vertices(len(points))
+    editor.init_cells(len(cells))
+    for k, point in enumerate(points):
+        editor.add_vertex(k, point[:2])
+    for k, cell in enumerate(cells.astype(numpy.uintp)):
+        editor.add_cell(k, cell)
+    editor.close()
+    Eps = numpy.array([[2.0, 1.0], [1.0, 2.0]])
+
+    solve(mesh, Eps, degree=1)
